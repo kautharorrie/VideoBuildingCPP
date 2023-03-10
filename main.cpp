92a84813d2a84acc1fc6446aaa1a7626ce967336
kautharorrie <orrkau001@myuct.ac.za>
2023-03-10 10:54:08 +0200

    testing the file for any bugs when extracting a frame. Not working as yet

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <sstream>
 #include <vector>
 
+
 using namespace std;
 int main (int argc, char** argv)
 {
@@ -23,10 +24,13 @@ int main (int argc, char** argv)
     int height = 0; 
     std::string greyscale = "";
     int val = 0;
-
+    int blocksize = 0;
 
     streampos size;
     char * memblock;
+
+    char * mem;
+    
 
     ifstream file("sloan_image.pgm", ios::in|ios::binary);
     if (file.is_open())
@@ -43,52 +47,74 @@ int main (int argc, char** argv)
 
         getline(file >> std::ws, greyscale);
         std::cout << "---------------- type: " << greyscale << std::endl;
-
+        
+        
+        
         int g = widthheight.find(" ");
         std::string w = widthheight.substr(0, g);
         std::string h = widthheight.substr(g+1, widthheight.length());
 
         width = atoi(w.c_str());
-        height = atoi(h.c_str());       
-            //size = file.tellg();
-        memblock = new char [4130*1968];
+        height = atoi(h.c_str());
+        
+        blocksize = width*height;
+        //size = file.tellg();
+        memblock = new char [blocksize];
         //file.seekg (0, ios::beg); //get the first position
-        file.read (memblock, 4130*1938);
+        file.read (memblock, blocksize);
         file.close();
 
-        cout << "the entire file content is in memory";
+        cout << "the entire file content is in memory" << std::endl;
 
 
     }
-    else {cout << "Unable to open file";}
+    else {cout << "Unable to open file" << std::endl;}
 
-    int g = widthheight.find(" ");
-    std::string w = widthheight.substr(0, g);
-    std::string h = widthheight.substr(g+1, widthheight.length());
-
-    width = atoi(w.c_str());
-    height = atoi(h.c_str());
      
+    std::cout << blocksize << std::endl;
+
+    char values[height][width];
+	int index = 0;
+
+    for (int x = 0; x < height; x++ )
+    {
+        for (int y = 0; y <width; y++ )
+        {
+
+            values[x][y] = memblock[index];
+            index++; 
+        }
+    }
+
+    mem = new char [blocksize];
+    int in = 0;
+    for (int x =0; x < 1938; x++ )
+    {
+        for (int y = 0; y <3000; y++ )
+        {
+            mem[in] = values[x][y];
+            
+            in++;
+        }
+    }
+
+    ofstream File("image.pgm", ios::in|ios::binary);
+    if (File.is_open())
+    {
+        //file.seekg (0, ios::beg); //get the first position
+        File << fileType << std::endl;
+        File << comments<< std::endl;;
+        File << widthheight << std::endl;;
+        File << greyscale << std::endl;;
+        File.write(mem, blocksize);
+        File.close();
+
+    }
+    else {cout << "Unable to open file" << std::endl;}
+
     
 
 
-	
-    // for (int x =0; x < height; x++ )
-    // {
-    //     for (int y = 0; y <width; y++ )
-    //     {
-    //         in >> val;
-    //         values[x][y] = val; 
-    //     }
-    // }
-
-    // // for (int x =0; x <2; x++ )
-    // // {
-    // //     for (int y = 0; y <6; y++ )
-    // //     {
-    // //         std::cout << values[x][y] << std::endl;
-    // //     }
-    // // }
 
 
 	std::cout << "Done." << std::endl;
@@ -104,7 +130,7 @@ int main (int argc, char** argv)
         std::cout << memblock[i] << " ";
     }
     delete[] memblock;
-
+    delete[] mem;
     return 0;
 }
 
