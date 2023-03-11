65470ffb5d9f69818da1aa9c94646738e1317155
kautharorrie <orrkau001@myuct.ac.za>
2023-03-11 18:29:07 +0200

    added namespace to header file, no functions added yet, and included the header file in the cpp file

---
+++ FrameSequence.cpp
@@ -0,0 +1,2 @@
+#include "FrameSequence.h"
+
---
+++ FrameSequence.h
@@ -0,0 +1,27 @@
+/**
+ * .h file 
+ * Simple Tag Enumeration Assignment 1 
+ * CSC3022F 
+ * Author: Kauthar Orrie
+ *
+ */
+ 
+#ifndef FRAMESEQUENCE_H
+#define FRAMESEQUENCE_H
+
+//includes 
+#include <string.h>
+#include <iostream>
+#include <ctype.h>
+#include <stdio.h>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <cstdlib>
+
+//declaring my namespace with my student number
+namespace ORRKAU001
+{
+}
+
+#endif
