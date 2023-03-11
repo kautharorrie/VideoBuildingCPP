de340e47b229d9ee0257021b90d745821252aca8
kautharorrie <orrkau001@myuct.ac.za>
2023-03-11 19:14:29 +0200

    Added the implemenation of the non, reverse, invert, revinvert features. No functionality yet.

--- FrameSequence.cpp
+++ FrameSequence.cpp
@@ -9,3 +9,22 @@ void ORRKAU001::FrameSequence::helloworld()
     std::cout << "Hello World!" << std::endl;
 }
 
+void ORRKAU001::FrameSequence::none(void)
+{
+    std::cout << "none" << std::endl;
+}   
+
+void ORRKAU001::FrameSequence::reverse(void)
+{
+    std::cout << "reverse" << std::endl;
+}
+
+void ORRKAU001::FrameSequence::invert(void)
+{
+    std::cout << "invert" << std::endl;
+}
+
+void ORRKAU001::FrameSequence::revinvert(void)
+{
+    std::cout << "revinvert" << std::endl;
+}
